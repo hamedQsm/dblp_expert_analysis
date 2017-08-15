575a54d94a953130be5fbed7e934a4345f548cf1
Hamed <hamed.ghasemie@gmail.com>
2017-08-15 23:57:42 +0200

    added the automatic downloading and extraction of dblp xml file

--- read_dblp_XML.py
+++ read_dblp_XML.py
@@ -1,6 +1,21 @@
 from DblpXmlHandler import *
+import urllib.request
+import gzip
+import shutil
+
 
 if __name__ == '__main__':
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists('data/dblp.xml'):
+        print('Downloading the dblp xml...')
+        urllib.request.urlretrieve('http://dblp.uni-trier.de/xml/dblp.xml.gz', 'data/dblp.xml.gz')
+        urllib.request.urlretrieve('http://dblp.uni-trier.de/xml/dblp.dtd', 'data/dblp.xml')
+        print('Extracting the zip file...')
+        with gzip.open('data/dblp.xml.gz', 'rb') as f_in, open('dblp.xml', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        print('Download and extraction finished.')
+
     dblp_handler = DblpXmlHandler()
     dblp_df = dblp_handler.convert2dataframe('data/dblp.xml')
     dblp_df = dblp_handler.merge_csv_files('tmp')
